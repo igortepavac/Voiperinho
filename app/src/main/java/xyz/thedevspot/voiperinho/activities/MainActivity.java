8ef02380efe563bc005787a2e39e6fdc658242b5
Igor Tepavac <igotepava@foi.hr>
2016-01-11 06:55:53 +0100

    Search Activity added

--- app/src/main/java/xyz/thedevspot/voiperinho/activities/MainActivity.java
+++ app/src/main/java/xyz/thedevspot/voiperinho/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.thedevspot.voiperinho.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -45,23 +46,18 @@ public class MainActivity extends BaseActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            startActivity(new Intent(this, SearchActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/xyz/thedevspot/voiperinho/activities/SearchActivity.java
@@ -0,0 +1,41 @@
+package xyz.thedevspot.voiperinho.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import xyz.thedevspot.voiperinho.R;
+import xyz.thedevspot.voiperinho.adapters.ContactsAdapter;
+
+public class SearchActivity extends BaseActivity {
+
+    @Bind(R.id.search_list)
+    ListView searchListView;
+
+    @Bind(R.id.search_go)
+    Button searchGo;
+
+    @Bind(R.id.search_username)
+    EditText searchUsername;
+
+    private ContactsAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @OnClick(R.id.search_go)
+    protected void onClickGo() {
+
+    }
+}
